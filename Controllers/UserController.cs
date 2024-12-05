587633ea4943c8750c17e082c5fd211b44805c0c
evgeny <wxnnvdie@gmail.com>
2024-12-05 12:09:24 +0500

    Сделана регистрация и авторизация

---
+++ Controllers/UserController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using registration;
+using registration.Classes;
+
+namespace JSONAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("ListAllUsers")]
+        public ActionResult<Person> GetAllListUsers()
+        {
+            var users = FileSingleton.deserializeCoolection<Person>(FileSingleton.pathToFileWithUsers);
+            if (users == null || users.Count == 0)
+            {
+                return NotFound("Пользователи не найдены");
+            }
+            return Ok(users);
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Person> GetUserById(int id)
+        {
+            var userByID = FileSingleton.deserializeCoolection<Person>(FileSingleton.pathToFileWithUsers).FirstOrDefault(u => id == u.Id);
+            return userByID == null ? NotFound() : Ok(userByID);
+        }
+
+        [HttpPost("Auth")]
+        public ActionResult<Person> Auth([FromBody] LoginRequest loginRequest)
+        {
+            var allUsers = FileSingleton.deserializeCoolection<Person>(FileSingleton.pathToFileWithUsers);
+            var user_check = allUsers.FirstOrDefault(u => u.email == loginRequest.Email);
+            if(user_check == null )
+            {
+                return NotFound("Не верный логин или пароль!");
+            }
+            if (user_check.timeBan > DateTime.Now)
+            {
+                return BadRequest("Пользователь заблокирован попробуйте позже");
+            }
+            user_check = allUsers.FirstOrDefault(u => u.email == loginRequest.Email && u.password == loginRequest.Password);
+            if (user_check == null)
+            {
+                user_check = allUsers.FirstOrDefault(u => u.email == loginRequest.Email);
+                user_check.failTry += 1;
+                userBanAndSerilize(user_check, FileSingleton.pathToFileWithUsers, allUsers);
+                return NotFound("Не верный логин или пароль!");
+            }
+
+            return Ok(user_check);
+        }
+
+        [HttpPost("Registration")]
+        public ActionResult<Person> Registration([FromBody]Person person)
+        {
+            var users = FileSingleton.deserializeCoolection<Person>(FileSingleton.pathToFileWithUsers);
+            if(users.FirstOrDefault(u => u.email == person.email) != null)
+            {
+                return BadRequest("Ошибка регистрации: данный email уже зарегистрирован!");
+            }
+            person.Id = users.Count>0 ? users.Max(u => u.Id)+1 : 1;
+
+            users.Add(person);
+            FileSingleton.serializeCollection<Person>(FileSingleton.pathToFileWithUsers, users);
+            return Ok(person);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            
+        }
+
+        private void userBanAndSerilize(Person user, string userFilePath, List<Person> allUsers)
+        {
+            if (user.failTry == 10)
+            {
+                user.isBanned = true;
+                user.timeBan = DateTime.Now.AddMinutes(30);
+                user.failTry = 0;
+            }
+            FileSingleton.serializeCollection(userFilePath, allUsers);
+
+        }
+    }
+}
---
+++ Helpers/FileSingleton.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace registration.Classes
+{
+    public class FileSingleton
+    {
+        private static FileSingleton instance;
+        public static string pathToFileWithUsers = "Data/allUsers.json";
+        public static string pathToFileWithPizza = "PizzaProducts.json";
+        public static string pathToFileWithBurgers = "BurgersProducts.json";
+        public static string pathToFileWithDrinks = "DrinksProducts.json";
+
+        private FileSingleton()
+        { }
+
+        public static FileSingleton getInstance()
+        {
+            if (instance == null) instance = new FileSingleton();
+            return instance;
+        }
+        public static void serializeCollection<T>(string pathToFile, IEnumerable<T> collection)
+        {
+            if (!File.Exists(pathToFile))
+            {
+                File.Create(pathToFile);
+            }
+            string data = JsonConvert.SerializeObject(collection, Formatting.Indented);
+            File.WriteAllText(pathToFile, data);
+        }
+        public static List<T> deserializeCoolection<T>(string pathToFile)
+        {
+            if (!File.Exists(pathToFile))
+            {
+                File.Create(pathToFile);
+            }
+            string data = File.ReadAllText(pathToFile);
+            if(data.Length == 0)
+            {
+                return [];
+            }
+            return JsonConvert.DeserializeObject<List<T>>(data);
+        }
+    }
+}
